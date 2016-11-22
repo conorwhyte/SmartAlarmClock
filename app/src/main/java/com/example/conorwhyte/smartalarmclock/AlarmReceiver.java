c88fc29206d6adda0765ea8c9d73919575f3bc57
padraigmitchell <padraig.mitchell@ucdconnect.ie>
2016-11-22 09:23:57 +0000

    CardListActivity now opens when alarm is set This is done in the in alarm reciever Has to be tidied up yet!

--- app/src/main/java/com/example/conorwhyte/smartalarmclock/AlarmReceiver.java
+++ app/src/main/java/com/example/conorwhyte/smartalarmclock/AlarmReceiver.java
@@ -11,23 +11,34 @@ import android.widget.Toast;
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
+   //Intent i;
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Log.e("WE are in the reciever.", "teat");
-        String state = intent.getExtras().getString("extra");
-
-        String richard_id = intent.getExtras().getString("quote id");
-
-        Intent service_intent = new Intent(context, RingtonePlayingService.class);
-        service_intent.putExtra("extra", state);
+       //String state = intent.getExtras().getString("extra");
+       // String richard_id = intent.getExtras().getString("quote id");Toast.makeText(context, "Recieved!!", Toast.LENGTH_LONG).show();
+        //i = new Intent(context,DistanceActivity.class );
+        //i.setClass(context, DistanceActivity.class); //Test is a dummy class name where to redirectVITY_NEW_TASK);
+        //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //i.putExtra("msg", richard_id);
+        Intent i = new Intent();
+        //i.setClassName("com.example.conorwhyte.smartalarmclock", "DistanceActivity.class");
+        i.setClassName("com.example.conorwhyte.smartalarmclock", "com.example.conorwhyte.smartalarmclock.CardListActivity");
+        // i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+        /**  Intent service_intent = new Intent(context, RingtonePlayingService.class);
+        context.startActivity(i);service_intent.putExtra("extra", state);
         service_intent.putExtra("quote id", richard_id);
-        context.startService(service_intent);
+        context.startService(service_intent);    **/
 
 
-        Log.e("MyActivity", "In the receiver with " + state);
+
+
+
+
+        //Log.e("MyActivity", "In the receiver with " + state);
 
 
 
