01d8c127d4907586816506c599def8c0548bc6ac
conorwhyte <conor.whyte@ucdconnect.ie>
2016-11-27 13:02:45 +0000

    Fixed Bug when user doesn't enter any card details

--- app/src/main/java/com/example/conorwhyte/smartalarmclock/AddUserDetailsActivity.java
+++ app/src/main/java/com/example/conorwhyte/smartalarmclock/AddUserDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.example.conorwhyte.smartalarmclock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -26,14 +29,17 @@ public class AddUserDetailsActivity extends AppCompatActivity {
         String morningName = editText.getText().toString();
 
         EditText editText1 = (EditText) findViewById(R.id.editText3);
-        //String timeNeeded = editText.getText().toString();
+        String timeNeeded = editText.getText().toString();
 
-        int timeRequired = Integer.parseInt(editText1.getText().toString());
-
-
-        count++ ;
-        name.add(morningName);
-        time.add(timeRequired);
+        if(editText1.getText().toString().trim().length() == 0 || editText.getText().toString().trim().length() == 0){
+            popUpEmptyTextView();
+        }
+        else {
+            int timeRequired = Integer.parseInt(editText1.getText().toString());
+            count++ ;
+            name.add(morningName);
+            time.add(timeRequired);
+        }
 
         /*Intent i = new Intent(getApplicationContext(), AddUserDetailsActivity.class);
         finish();
@@ -43,8 +49,6 @@ public class AddUserDetailsActivity extends AppCompatActivity {
 
     UserDetails newUser = new UserDetails();
     public void sendDetails(View view){
-
-
         newUser.setCardWakeTime(time);
         newUser.setCardName(name);
         newUser.setNumber(count);
@@ -54,5 +58,25 @@ public class AddUserDetailsActivity extends AppCompatActivity {
         i.putExtra("Object", newUser);
         startActivity(i);
         finish();
+    }
+
+
+    public void popUpEmptyTextView() {
+        final Intent intent = new Intent(this, AddUserDetailsActivity.class);
+        AlertDialog alertDialog = new AlertDialog.Builder(AddUserDetailsActivity.this).create();
+        alertDialog.setTitle("ERROR");
+        alertDialog.setMessage("One of the fields have been left empty, please try again."
+                );
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        //Intent intent = new Intent(this, AddUserDetailsActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                });
+        alertDialog.show();
     }
 }
