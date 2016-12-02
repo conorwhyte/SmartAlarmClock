36c6226488148dd5c6fb5f45500ba057bdc58968
padraigmitchell <padraig.mitchell@ucdconnect.ie>
2016-12-02 00:32:30 +0000

    New user details references and all cardlist features

--- app/src/main/java/com/example/conorwhyte/smartalarmclock/MainActivity.java
+++ app/src/main/java/com/example/conorwhyte/smartalarmclock/MainActivity.java
@@ -1,25 +1,13 @@
 package com.example.conorwhyte.smartalarmclock;
 
-import android.annotation.TargetApi;
-import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Build;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
